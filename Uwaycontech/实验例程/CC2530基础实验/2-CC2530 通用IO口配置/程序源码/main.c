4fdcba3e8d729009f5eefa464015d8ae6e8d869b
Shillcly <1853916542@qq.com>
2025-07-19 16:18:58 +0800

    更新

---
+++ Uwaycontech/实验例程/CC2530基础实验/2-CC2530 通用IO口配置/程序源码/main.c
@@ -0,0 +1,35 @@
+#include<iocc2530.h>                     //头文件
+
+void delayms(unsigned int xms)          //ms级别延时
+{
+    for(;xms>0;xms--)
+      for(unsigned int j=587;j>0;j--);
+}
+
+void main(void)
+{
+    
+    P1SEL &= ~0x07;                     //配置P1_2-P1_0为通用IO口
+    P1DIR |= 0x07;                      //配置P1_2-P1_0为输出模式
+    P1 = 0x00;                          //配置P1_2-P1_0为低电平
+    
+    P0SEL &= ~0x01;                     //配置P0_0为通用IO口
+    P0DIR |= 0x00;                      //配置P0_0为输入模式
+    P0INP |= 0x00;                      //配置P0_0为上、下拉输入模式
+    while(1)
+    {
+        if(P0_0 == 0)                   //检测到按键1被是否按下
+        {
+            delayms(10);                //按键按下时进行一次5-20ms的延时
+            while(P0_0 == 0);           //当按键未被松开时进入死循环
+            delayms(10);                //按键释放时进行一次5-20ms的延时
+            P1 = ~P1;                   //让P1组IO口的电平翻转
+        }
+    }
+}
+
+
+
+
+
+
