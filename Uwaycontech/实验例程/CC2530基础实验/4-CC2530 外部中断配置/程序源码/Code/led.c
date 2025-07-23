21120ceb62fae3a639078c079151ba5db6dddfb2
Shillcly <1853916542@qq.com>
2025-07-23 16:52:35 +0800

    更新

---
+++ Uwaycontech/实验例程/CC2530基础实验/4-CC2530 外部中断配置/程序源码/Code/led.c
@@ -0,0 +1,19 @@
+#include "Headfile.h"
+//对LED的三个引脚进行初始化
+void led_init(void)
+{
+    P1SEL &= ~0x07;
+    P1DIR |= 0x07;
+    P1 = 0x00;
+}
+//LED的亮灭模块，参数一为LED的位数，参数二为LED 的状态
+void led_proc(uint8_t led,uint8_t light)
+{
+    switch(led)
+    {
+        case 1:LED1 = light;break;
+        case 2:LED2 = light;break;
+        case 3:LED3 = light;break;
+        default :break;
+    }
+}
